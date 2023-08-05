df5e8a3c147789267c5560cf205ac788b5bae829
tangwai <tmichael11314@gmail.com>
2023-08-04 19:10:22 -0500

    add another loop for rows/col boxes add const for rows/cols boxes to support up to 8x8 grid

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-const boxes = (numBoxes) => {
-    const container = document.querySelector('#boxes-container');
-    for (let i = 0; i < numBoxes; i++) {
-        const div = document.createElement('div');
-        div.classList.add('boxes');
-        container.appendChild(div);
-    }
-        const boxElements = document.querySelectorAll('boxes');
-        boxElements.forEach(box => {
-            box.addEventListener('mouseenter', function() {
-                box.style.backgroundColor = 'blue';
-        });
+document.addEventListener("DOMContentLoaded", function() {
+    const boxes = (numRows, numCols) => {
+        const container = document.querySelector('#boxes-container');
+        for (let i = 0; i < numRows; i++) {
+            for (let j = 0; j < numCols; j++) {
+                const div = document.createElement('div');
+                div.classList.add('boxes');
+                container.appendChild(div);
+
+                div.addEventListener('mouseenter', function() {
+                    div.style.backgroundColor = 'blue';
+                });
+            }
+        }
+    };
+
+    const slider = document.getElementById('myRange');
+    slider.addEventListener('input', function() {
+        const numBoxes = parseInt(slider.value);
+        const numRows = Math.ceil(Math.sqrt(numBoxes));
+        const numCols = Math.ceil(Math.sqrt(numBoxes));
+        
+        const container = document.querySelector('#boxes-container');
+        container.innerHTML = '';
+        boxes(numRows, numCols);
     });
-};
 
-const slider = document.getElementById('myRange');
-slider.addEventListener('input', function() {
-    const numBoxes = parseInt(slider.value);
-    const container = document.querySelector('#boxes-container');
-    container.innerHTML = '';
-    boxes(numBoxes);
+    boxes(4, 4);
 });
-
-boxes(16);
-
